b3982e3394714a5edaf38ea5e6fe2d5054d9a57f
Kabirah Azeez <kabirahazeez692@gmail.com>
2023-08-10 13:15:07 -0400

    Initial Commit

---
+++ Practice/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Practice
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var day = ""
+    @State private var textTitle = ""
+    var body: some View {
+        ZStack {
+            VStack {
+                Image("me")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.leading)
+                Text(textTitle)
+                
+                Button("Click Here to Learn About Me"){ textTitle = "I am a rising junior who is interested in pursuing CS. I love thrifting and going out to eat at new restaurants! "
+                    
+                    
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint (Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/))
+            }
+        }
+        .padding()
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
